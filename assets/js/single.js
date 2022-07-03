e032c7dc604a3e595f20e4dc328e80c542e30b6e
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-07-02 22:41:22 -0500

    handling error issues. index redirect

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,20 @@
 var issuesContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+}
+    else {
+        document.location.replace("./index.html");
+    
+}
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -11,30 +26,23 @@ var getRepoIssues = function(repo) {
                 // console.log(data);
                 // pass response data to dom function
                 displayIssues(data);
-            });
+            
                 // check if api has paginated issues
                 if(response.headers.get("Link")) {
-                    var displayWarning = function(repo) {
-                        limitWarningEl.textContent = "To see more than 30 issues, visit ";
-                        var linkEl = document.createElement("a");
-                        linkEl.textContent = "See More Issues On GitHub.com";
-                        linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
-                        linkEl.setAttribute("target", "_blank");
-
-                        // append to warning container
-                        limitWarningEl.appendChild(linkEl);
-                    };
                     // console.log("repo has more than 30 issues");
                     displayWarning(repo);
                 }
+            });
+
+        }else {
+        document.location.replace("./index.html");
         }
-        else {
-            alert("There was a problem with your request!");
-        }
-        
-    });
+});
 };
-getRepoIssues("angular/angular");
+
+// getRepoIssues();
+
+
 
 // Turn Github issue data in DOM elements
 var displayIssues = function(issues) {
@@ -59,7 +67,7 @@ var displayIssues = function(issues) {
 
         // create a type element
         var typeEl = document.createElement("span");
-        
+    
         // check if issue is an actual issue or a pull request
         if (issues[i].pull_request) {
             typeEl.textContent = "(Pull request)";
@@ -73,5 +81,17 @@ var displayIssues = function(issues) {
 
         // append to html
         issuesContainerEl.appendChild(issueNameEl);
-    }
-};
+        }
+    };
+
+    var displayWarning = function(repo) {
+        limitWarningEl.textContent = "To see more than 30 issues, visit ";
+        var linkEl = document.createElement("a");
+        linkEl.textContent = "See More Issues On GitHub.com";
+        linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+        linkEl.setAttribute("target", "_blank");
+
+        // append to warning container
+        limitWarningEl.appendChild(linkEl);
+    };
+    getRepoName();
